a65bbe1458cb17662a01e5185344f52181aed380
advmindev <abbey.anke@gmail.com>
2016-10-30 10:38:24 +0900

    update ansibles

--- bin/secretmount-abbeyanke.sh
+++ bin/secretmount-abbeyanke.sh
@@ -1,3 +1,2 @@
-encfs -o nonempty ~/Documents/settings/0000-GitHubData ~/Documents/0000-GitHub
-
-
+# sudo this with option --public will share 0000-GitHub directory for everyone
+encfs --nocache $* -o nonempty ~/Documents/settings/0000-GitHubData ~/Documents/0000-GitHub
