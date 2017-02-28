b41e0d51155d652f0e9e7ca7e88126f4a5a311b7
suz <tadashi.suzue@konicaminolta.com>
2017-02-28 11:51:18 +0900

    update

--- bin/fixing-startup-issue-on-wifi.sh
+++ bin/fixing-startup-issue-on-wifi.sh
@@ -1,3 +1,10 @@
-sudo ifconfig wlp4s0:0 192.168.10.6
+sudo ifconfig enp0s31f6 192.168.10.2/27
+sudo ifconfig wlp4s0:0 192.168.10.39/27
 systemctl.sh
 sudo systemctl restart smbd
+sudo swapon /root/.SWAPFILE
+sudo systemctl restart NetworkManager
+sleep 10
+sudo ifconfig enp0s31f6 192.168.10.2/27
+sudo ifconfig wlp4s0:0 192.168.10.39/27
+sudo supervisorctl restart fess
