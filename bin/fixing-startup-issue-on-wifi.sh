3ef71a063c94a549084c3fec893b7806afb1ee52
suz <tadashi.suzue@konicaminolta.com>
2017-09-25 09:07:49 +0900

    add fmeo

--- bin/fixing-startup-issue-on-wifi.sh
+++ bin/fixing-startup-issue-on-wifi.sh
@@ -1,10 +1,28 @@
-sudo ifconfig enp0s31f6 192.168.10.2/27
-sudo ifconfig wlp4s0:0 192.168.10.39/27
+#!/bin/bash
+
+function eth_available() {
+    connected=`cat /sys/class/net/enp0s31f6/carrier`
+    if [ "$connected"x = "1"x ]; then
+        sudo ifconfig enp0s31f6 192.168.10.5/27
+        #sudo ifconfig enp0s31f6:0 192.168.10.2/27
+    fi
+}
+
+function wifi_available() {
+    connected=`cat /sys/class/net/wlp4s0/carrier`
+    if [ "$connected"x = "1"x ]; then
+	sudo ifconfig wlp4s0:0 192.168.10.39/27
+    fi
+}
+
+eth_available
+wifi_available
 systemctl.sh
 sudo systemctl restart smbd
 sudo swapon /root/.SWAPFILE
 sudo systemctl restart NetworkManager
 sleep 10
-sudo ifconfig enp0s31f6 192.168.10.2/27
-sudo ifconfig wlp4s0:0 192.168.10.39/27
+eth_available
+wifi_available
 sudo supervisorctl restart fess
+systemctl.sh
